151a6226885708185d7f0869597e5006d9484dea
Sota0531 <na.3105.so@icloud.com>
2024-12-14 16:29:25 +0900

    ライフ処理を追加

---
+++ Assets/Scripts/PlayerLifeManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerLifeManager : MonoBehaviour
+{
+    private int maxLife = 5;
+    private int nowLife = 5;
+
+    [SerializeField] TMP_Text _LifeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _LifeText.text = "Life:" + nowLife.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddLife(int addnum){
+        if(nowLife < maxLife){
+            if(nowLife + addnum <= maxLife){
+                nowLife += addnum;
+                
+            }else{
+                nowLife = maxLife;
+            }
+            _LifeText.text = "Life:" + nowLife.ToString();
+        }
+    }
+    public void RemoveLife(int removenum){
+        nowLife -= removenum;
+        if(nowLife <= 0){
+            //TODO: ゲームオーバー処理
+        }else{
+            _LifeText.text = "Life:" + nowLife.ToString();
+        }
+    }
+}
