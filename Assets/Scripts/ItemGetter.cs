80f6170f0173423e2d28cd203c7365e105d71ba7
Sota0531 <na.3105.so@icloud.com>
2024-12-14 17:24:47 +0900

    障害物激突処理を追加

--- Assets/Scripts/ItemGetter.cs
+++ Assets/Scripts/ItemGetter.cs
@@ -6,11 +6,20 @@ using UnityEngine;
 public class ItemGetter : MonoBehaviour
 {
     [SerializeField] GameObject scoreText;
+    [SerializeField] GameObject HPText;
+    [SerializeField] GameObject Player;
     void OnTriggerEnter2D(Collider2D col){
+        //コイン取得処理
         if(col.gameObject.TryGetComponent<ICoin>(out var coin)){
             int num = coin.GetCoinScore();
             coin.DestroyThisObject();
             scoreText.GetComponent<TotalScoreManager>().AddScore(num);
+        }
+        //障害物激突処理
+        if(col.gameObject.TryGetComponent<IObstacle>(out var obstacle)){
+            int num = obstacle.DecreaseHP();
+            HPText.GetComponent<PlayerLifeManager>().RemoveLife(num);
+            SetObjectCollisionActive.SetCollisionActive(false,Player,col.gameObject);
         }
     }
 }
