9851be1be7f548feea9bd44c1f0a0b8c4e531564
Sota0531 <na.3105.so@icloud.com>
2024-12-14 18:00:19 +0900

    OldStageDestroyerをデバッグ

--- Assets/Scripts/OldStageDestroyer.cs
+++ Assets/Scripts/OldStageDestroyer.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 
 public class OldStageDestroyer : MonoBehaviour
 {
+    [SerializeField] GameObject playerObj;
+    private Vector3 offset;
+    void Start(){
+        offset = transform.position - playerObj.transform.position;
+    }
     private void OnTriggerExit2D(Collider2D col){
         //GameObject gameObject= col.gameObject;
         //Destroy(gameObject);
         Destroy(col.gameObject.transform.parent.gameObject);
+    }
+    void LateUpdate()
+    {
+        // カメラのY座標を固定しながら追従
+        transform.position = new Vector3(playerObj.transform.position.x + offset.x, playerObj.transform.position.y + offset.y,playerObj.transform.position.z + offset.z);
     }
 }
